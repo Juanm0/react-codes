8adfe5cbf5ca9a8c3b6a825c21c089bc00edc0e4
Juanm0 <juanm.navarro05@gmail.com>
2026-04-26 15:03:02 -0300

    que pasaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 
 import Navbar from "./components/Navbar"
-import ItemListContainer from './components/ItemListContainer'
+
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavbarRB from "./components/NavbarRB";
-import ItemCount from "./components/ItemCount";
 import DinamicComponent from "./examples/DinamicComponent";
-import ComponentChildren from "./examples/ComponentChildren";
+
 
 function App() {
  
@@ -15,8 +13,9 @@ console.log('App')
   return (
     <>
      
-     <NavbarRB/>
-     <ItemListContainer saludo="Bienvenidos a mi App!" />
+     
+     
+     <DinamicComponent></DinamicComponent>
       {/* <ItemCount stock={5}/> */}
      {/* <ComponentChildren>
       Hola miren soy un children
