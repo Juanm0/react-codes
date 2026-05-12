5ba956548adae5b9f19b311393bbcc130cb1dbb5
Juanm0 <juanm.navarro05@gmail.com>
2026-05-12 18:37:33 -0300

    vamos a consumir una api real

---
+++ src/examples/FetchContainer.jsx
@@ -0,0 +1,12 @@
+import React, {useEffect} from 'react'
+
+const FetchContainer = () => {
+    useEffect(()=>{
+        
+    },[])
+  return (
+    <div>FetchContainer</div>
+  )
+}
+
+export default FetchContainer
