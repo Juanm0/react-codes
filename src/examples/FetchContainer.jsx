eeddf34f09d1b4284d72fcd934259935d12dfd7d
Juanm0 <juanm.navarro05@gmail.com>
2026-05-12 19:02:36 -0300

    metemos la api de pokemon

--- src/examples/FetchContainer.jsx
+++ src/examples/FetchContainer.jsx
@@ -2,7 +2,8 @@ import React, {useEffect} from 'react'
 
 const FetchContainer = () => {
     useEffect(()=>{
-        
+        fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+        .then((response)=> console.log(response))
     },[])
   return (
     <div>FetchContainer</div>
