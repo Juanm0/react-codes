9d83e1fdff181613b0b3bb467010ed393df9e5f3
Juanm0 <juanm.navarro05@gmail.com>
2026-05-13 16:18:43 -0300

    mi custom hook no funciona, falta crear el detalle de producto para la clase 5 solamente

--- src/examples/FetchContainer.jsx
+++ src/examples/FetchContainer.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import FetchList from './FetchList'
+/* import {useFetch} from '../hooks/useFetch' */
 
 const FetchContainer = () => {
+  /* acá usamos un custom hook pero como es complicado mejor no lo usamos
+const {data, error, loading} = useFetch('https://pokeapi.co/api/v2/pokemon?limit=50')
+*/
   const [data, setData] = useState([])
-  useEffect(() => {
+   useEffect(() => {
     //1. Pido datos
     fetch('https://pokeapi.co/api/v2/pokemon?limit=50')
       //2. Traduzco
@@ -13,7 +17,10 @@ const FetchContainer = () => {
       //4. Atrapo los errores
       .catch((error) => console.log(error))
   }, [])
-  console.log(data);
+
+  /*console.log({data, error, loading});  */
+  
+  console.log(data); 
 
   return (
     <div>FetchContainer
