c7b210c596a8f4db1f230773f47f1a59f8cb96db
Juanm0 <juanm.navarro05@gmail.com>
2026-05-30 13:51:52 -0300

    cambiamos las imagenes de itemdetail asi son mas chicas

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -27,7 +27,7 @@ console.log(cart, 'contexto')
         gap: '10px'
       }}>
         <h2>Bienvenidos al detalle de {detalle.name}</h2>
-        <img src={detalle.img} alt={detalle.name}/>
+        <img width='300px' src={detalle.img} alt={detalle.name} />
         <p>{detalle.description}</p>
         <p>${detalle.price},00</p>
         <p>unidades disponibles: {detalle.stock}</p>
